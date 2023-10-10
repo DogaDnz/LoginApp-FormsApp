3458f51ad7cc2278606fa432a7a8291b2d380bc4
dogadeniz <121103575+DogaDnz@users.noreply.github.com>
2023-10-10 19:45:57 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp2
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.Username = new System.Windows.Forms.Label();
+			this.Password = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(400, 101);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(116, 76);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(168, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(116, 128);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(168, 20);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.UseSystemPasswordChar = true;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.button1.ForeColor = System.Drawing.Color.Snow;
+			this.button1.Location = new System.Drawing.Point(135, 165);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(118, 34);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Login";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Username
+			// 
+			this.Username.AutoSize = true;
+			this.Username.Location = new System.Drawing.Point(38, 83);
+			this.Username.Name = "Username";
+			this.Username.Size = new System.Drawing.Size(55, 13);
+			this.Username.TabIndex = 3;
+			this.Username.Text = "Username";
+			this.Username.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// Password
+			// 
+			this.Password.AutoSize = true;
+			this.Password.Location = new System.Drawing.Point(38, 135);
+			this.Password.Name = "Password";
+			this.Password.Size = new System.Drawing.Size(53, 13);
+			this.Password.TabIndex = 4;
+			this.Password.Text = "Password";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Password);
+			this.Controls.Add(this.Username);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ToolTip toolTip1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label Username;
+		private System.Windows.Forms.Label Password;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void Form1_DoubleClick(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string ka = textBox1.Text;
+			string pw = textBox2.Text;
+
+			if(ka =="Doga")
+			{
+				if(pw == "123")
+				{
+					label1.Text = "Successful";
+				}
+				else {
+					label1.Text = "Try again the password";
+				}
+			}
+			else {
+				label1.Text = "unSuccessful";
+			}
+		}
+	}
+}
